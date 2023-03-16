6ce2f0a6758c5ec55e1feceb27961df571c99832
jordansoy <jordansoy@gmail.com>
2023-03-16 20:17:27 +0100

    Remove unused content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          My Form
         </p>
         <a
           className="App-link"
