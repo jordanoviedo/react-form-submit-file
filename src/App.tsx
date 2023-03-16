94d8abbcae12e659fc0ea64ccb4ee52d7ba28b8e
jordansoy <jordansoy@gmail.com>
2023-03-16 20:37:03 +0100

    state variables for form elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const onChangeFile = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
